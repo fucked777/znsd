6c4eb84ac58eccf365a3f5070972698969fbc3a1
duhongxing <1273016161@qq.com>
2022-02-17 16:11:22 +0800

    提交
    提交

--- MDS_DevelopWork/src/UserPlugins/SecondMotionControlPlugin/secondmotioncontrolpluginactivator.h
+++ MDS_DevelopWork/src/UserPlugins/SecondMotionControlPlugin/secondmotioncontrolpluginactivator.h
@@ -1,40 +1,44 @@
-#ifndef SECONDMOTIONCONTROLPLUGINACTIVATOR_H 
-#define SECONDMOTIONCONTROLPLUGINACTIVATOR_H 
+#ifndef SECONDMOTIONCONTROLPLUGINACTIVATOR_H
+#define SECONDMOTIONCONTROLPLUGINACTIVATOR_H
 
+#include <HMPPluginActivator.h>
 #include <QObject>
 #include <QtPlugin>
-#include <HMPPluginActivator.h>
 
 class SecondMotionControlPluginService;
 
 class SecondMotionControlPluginActivator : public QObject, public HMPPluginActivator
-{ 
-   Q_OBJECT 
-   Q_INTERFACES(HMPPluginActivator)
+{
+    Q_OBJECT
+    Q_INTERFACES(HMPPluginActivator)
 
-   //Qt5 
-#if(QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
-   Q_PLUGIN_METADATA(IID "SecondMotionControlPluginActivator" SecondMotionControlPluginActivator)
+    // Qt5
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
+    Q_PLUGIN_METADATA(IID "SecondMotionControlPluginActivator" SecondMotionControlPluginActivator)
 #endif
 
 public:
-   SecondMotionControlPluginActivator():m_pService(NULL),m_pContext(NULL) {}
+    SecondMotionControlPluginActivator()
+        : m_pService(NULL)
+        , m_pContext(NULL)
+    {
+    }
 
 public:
-   /**
-    * @brief start     
-    * @param context   
-    */
+    /**
+     * @brief start
+     * @param context
+     */
     virtual void start(HMPPluginContext* context) override;
-   /**
-    * @brief stop      
-    * @param context   
-    */
+    /**
+     * @brief stop
+     * @param context
+     */
     virtual void stop(HMPPluginContext* context) override;
+
 private:
-   SecondMotionControlPluginService *m_pService;             
-   HMPPluginContext *m_pContext;     
-}; 
+    SecondMotionControlPluginService* m_pService;
+    HMPPluginContext* m_pContext;
+};
 
-#endif  // SECONDMOTIONCONTROLPLUGINACTIVATOR_H 
-
+#endif  // SECONDMOTIONCONTROLPLUGINACTIVATOR_H
